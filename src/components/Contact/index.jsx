aff52475ea6088aafde5ce789b891c810fd63603
Andy Bool <andybool50@gmail.com>
2024-10-07 12:36:35 +0100

    changed CV to See My CV in About section

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,8 +6,8 @@ export default function Contact() {
                 <a href="tel:+447941042227" className="hover:text-pink-500">Tel</a>&nbsp; | &nbsp;
                 <a href="https://www.linkedin.com/in/andy-bool-568a2622/" target="_blank" className="hover:text-pink-500">LinkedIn</a>&nbsp; | &nbsp;
                 <a href="https://github.com/AndyB0o1" target="_blank" className="hover:text-pink-500">Github</a>&nbsp; | &nbsp;
-                <a href="/AndyBoolCV.pdf" download className="sm:hidden hover:text-pink-500">CV</a>
-                <a href="/AndyBoolCV.pdf" download className="hidden sm:inline hover:text-pink-500">Download my CV</a>
+                <a href="/AndyBoolDevCV.pdf" download className="sm:hidden hover:text-pink-500">CV</a>
+                <a href="/AndyBoolDevCV.pdf" download className="hidden sm:inline hover:text-pink-500">Download my CV</a>
             </p>
         </div>
     )
